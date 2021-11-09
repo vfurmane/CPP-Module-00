962b9504a8c2c59084409c7476988717cd2ba240
Valentin <vfurmane@student.42.fr>
2021-11-09 11:46:56 +0100

    feat: [ex01] fix segfault when adding more than 8 contacts

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:46:13 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/11/08 15:23:55 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/11/09 11:35:30 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ void	PhoneBook::addNewContact(std::string first_name, std::string last_name,
 		std::string darkest_secret)
 {
 	if (this->_current_id >= 8)
+	{
 		memmove(this->_contacts, &this->_contacts[1],
 			7 * sizeof (*this->_contacts));
+		this->_current_id = 7;
+	}
 	this->_contacts[this->_current_id].updateInfo(first_name, last_name, nickname, phone_number, darkest_secret);
 	if (this->_current_id < 8)
 		this->_current_id++;
